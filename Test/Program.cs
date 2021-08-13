5dc402d7e52b0d5faf1abf89b18669641675b70b
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2021-08-13 01:17:40 -0400

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+using AutoNotify;
+
+namespace Test
+{
+    public partial class MyObject
+    {
+        [AutoNotifyProperty("Health")]
+        private int _health;
+        [AutoNotifyProperty("Name")]
+        private string _name;
+        [AutoNotifyProperty("Exp")]
+        private float  _exp;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var so = new MyObject();
+            so.PropertyChanged += (sender, ev) =>
+            {
+                Console.WriteLine(ev.PropertyName + " was modified!!!");
+            };
+            so.Health = 5;
+            so.Exp = 1f;
+            so.Name = "CatSandwich";
+        }
+    }
+}
